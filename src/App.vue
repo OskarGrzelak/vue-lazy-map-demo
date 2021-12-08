8973c7e639272193f46a786176dc2e1214083f15
Oskar Grzelak <oskar.grzelak@gmail.com>
2021-12-08 10:00:59 +0100

    add tailwind to project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container mx-auto">
     <map-static
       v-if="!isInteractive"
       :map="map"
--- src/components/MapStatic.vue
+++ src/components/MapStatic.vue
@@ -17,6 +17,5 @@ export default {
 <style scoped>
 .map {
   width: 100%;
-  height: 400px;
 }
 </style>
